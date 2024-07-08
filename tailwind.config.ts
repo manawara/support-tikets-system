07e964d7df90c47872c5a0e4b9e8aa3432c4991e
manawara <marcinnawara@int.pl>
2024-07-08 10:32:43 +0200

    Add palette colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        midnightBlue: '#41729F',
+        blueGray: '#5885AF',
+        darkBlue: '#274472',
+        babyBlue: '#C3E0E5',
+      },
+      fontFamily: {
+        lato: ['var(--font-lato)'],
+        poppins: ['var(--font-poppins)'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
