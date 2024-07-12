4a31094f1ce27e11df033509489032567242ab27
manawara <marcinnawara@int.pl>
2024-07-12 22:17:52 +0200

    Add Table component to the page

--- app/(dashboard)/tickets/page.tsx
+++ app/(dashboard)/tickets/page.tsx
@@ -1,11 +1,15 @@
 import FilterTicket from '@/components/FilterTicket/FilterTicket'
 import SidebarTicket from '@/components/SidebarTicket/SidebarTicket'
+import Table from '@/components/Table/Table'
 
 const page = () => {
   return (
     <div>
       <FilterTicket />
       <SidebarTicket />
+      <div className="px-8 bg-lightBlue">
+        <Table />
+      </div>
     </div>
   )
 }
