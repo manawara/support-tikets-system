db684a519a481b4bdef12e7adcda2c5efb956e59
manawara <marcinnawara@int.pl>
2024-07-22 17:11:47 +0200

    Provider credentials add to auth.config and  refactor auth.ts add new variable to adapter

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,52 @@
 import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
+import { ZodError } from 'zod'
+import Credentials from 'next-auth/providers/credentials'
+import bcrypt from 'bcryptjs'
 
 import type { NextAuthConfig } from 'next-auth'
+import { LoginSchema } from './schemas'
+import { getUserByEmail } from './data'
 
-export default { providers: [GitHub, Google] } satisfies NextAuthConfig
+export default {
+  providers: [
+    GitHub,
+    Google,
+    Credentials({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+      async authorize(credentials) {
+        try {
+          if (!credentials?.email || !credentials?.password) {
+            return null
+          }
+
+          const { email, password } = await LoginSchema.parseAsync(credentials)
+
+          const user = await getUserByEmail(email)
+
+          if (!user || !user.password) {
+            return null
+          }
+
+          const passwordMatch = await bcrypt.compare(password, user.password)
+
+          if (!passwordMatch) {
+            return null
+          }
+
+          return user
+        } catch (error) {
+          if (error instanceof ZodError) {
+            console.error('Validation error:', error.errors)
+          } else {
+            console.error('Authorization error:', error)
+          }
+          return null
+        }
+      },
+    }),
+  ],
+} satisfies NextAuthConfig
