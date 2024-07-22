a358c13c019e9bda6a10a72795da0c2dee9dc3c2
manawara <marcinnawara@int.pl>
2024-07-23 00:18:25 +0200

    Two new function  generateVerificationToken and getVerificationToken

--- data/index.ts
+++ data/index.ts
@@ -1,5 +1,7 @@
 import { db } from '@/lib/db'
 import { User } from 'next-auth'
+import { v4 as uuidv4 } from 'uuid'
+
 export const getUserByEmail = async (email: string) => {
   return await db.user.findFirst({
     where: {
@@ -16,4 +18,44 @@ export const addUser = async (fullName: string, email: string, hashedPassword: s
       password: hashedPassword,
     },
   })
+}
+
+//verificationtoken
+
+export const getVerificationToken = async (email: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({
+      where: {
+        email,
+      },
+    })
+    return verificationToken
+  } catch (err) {
+    return null
+  }
+}
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4()
+  const expires = new Date(new Date().getTime() + 3600 * 1000)
+  const existingToken = await getVerificationToken(email)
+  if (existingToken) {
+    await db.verificationToken.delete({
+      where: {
+        email_token: {
+          email: existingToken.email,
+          token: existingToken.token,
+        },
+      },
+    })
+  }
+  const verificationToken = await db.verificationToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  })
+
+  return verificationToken
 }
