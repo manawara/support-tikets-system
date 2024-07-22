784e3d1d58c7a6455a09092abbc7c1e407e7346e
manawara <marcinnawara@int.pl>
2024-07-23 00:14:40 +0200

    Add function generateVerificationToken after register user

--- actions/register.ts
+++ actions/register.ts
@@ -2,7 +2,7 @@
 import * as z from 'zod'
 import bcrypt from 'bcryptjs'
 import { RegisterSchema } from '@/schemas'
-import { addUser, getUserByEmail } from '@/data'
+import { addUser, generateVerificationToken, getUserByEmail } from '@/data'
 export const createUser = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
 
@@ -20,8 +20,10 @@ export const createUser = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Email already in use' }
   }
   await addUser(fullName, email, hashedPassword)
+  const verificationToken = await generateVerificationToken(email)
 
+  //todo sent email
   return {
-    success: 'The user has been created!',
+    success: 'Confirmation email sent!',
   }
 }
