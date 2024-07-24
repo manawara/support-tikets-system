9ee58d0b3eff9cd3b8bdf81c934b2846b9da403c
manawara <marcinnawara@int.pl>
2024-07-24 15:50:48 +0200

    Change adding data to Table with outside props - ticket page

--- app/dashboard/tickets/page.tsx
+++ app/dashboard/tickets/page.tsx
@@ -3,12 +3,65 @@ import SidebarTicket from '@/components/SidebarTicket/SidebarTicket'
 import Table from '@/components/Table/Table'
 
 const TicketPage = () => {
+  const data = [
+    {
+      fullName: 'Bill Gates',
+      topic: 'item return',
+      agent: 'Marcin',
+      status: 'Open',
+      id: 1111,
+    },
+    {
+      fullName: 'Bill Gates',
+      topic: 'item return',
+      agent: 'Marcin',
+      status: 'Open',
+      id: 2322,
+    },
+    {
+      fullName: 'Bill Gates',
+      topic: 'item return',
+      agent: 'Marcin',
+      status: 'Open',
+      id: 444,
+    },
+    {
+      fullName: 'Bill Gates',
+      topic: 'item return',
+      agent: 'Marcin',
+      status: 'Open',
+      id: 55,
+    },
+    {
+      fullName: 'Bill Gates',
+      topic: 'item return',
+      agent: 'Marcin',
+      status: 'Open',
+      id: 55,
+    },
+    {
+      fullName: 'Bill Gates',
+      topic: 'item return',
+      agent: 'Marcin',
+      status: 'Open',
+      id: 55,
+    },
+  ]
+
+  const columns = [
+    { key: 'id', header: 'ID' },
+    { key: 'fullName', header: 'Client' },
+    { key: 'topic', header: 'Topic' },
+    { key: 'agent', header: 'Agent' },
+    { key: 'status', header: 'Status' },
+    { key: 'action', header: '' },
+  ]
   return (
     <div>
       <FilterTicket />
       <SidebarTicket />
       <div className="px-8 bg-lightBlue">
-        <Table />
+        <Table data={data} columns={columns} namePage="tickets/" />
       </div>
     </div>
   )
