9dcf7ec44dbec2d58da6338550e419a010f56115
manawara <marcinnawara@int.pl>
2024-07-29 13:42:03 +0200

    Create new form and import components to dynamic EditUser page

--- app/dashboard/team/[id]/page.tsx
+++ app/dashboard/team/[id]/page.tsx
@@ -1,18 +1,29 @@
 import UserDetails from '@/components/UserDetails/UserDetails'
-import Card from '@/components/Card'
+import Permission from '@/components/UserDetails/Permission'
+import UserActivity from '@/components/UserDetails/UserActivity'
+import DeactivateUser from '@/components/UserDetails/DeactivateUser'
+import { Button } from '@/components/Button'
 const EditUser = () => {
   return (
-    <div>
-      <div className="flex gap-8">
+    <div className="mb-8">
+      <h2 className="text-2xl font-poppins mb-4">Edit User - Marcin Nawara</h2>
+
+      <div className="flex flex-col md:flex-row  gap-8">
         <div className="flex flex-col gap-4 grow">
-          <h2 className="text-2xl font-poppins text-center mb-4">Edit User - Marcin Nawara</h2>
-          <UserDetails>sdsdd</UserDetails>
-          <Card>
-            <h3>Permissions</h3>
-          </Card>
+          <form className="flex flex-col gap-8">
+            <UserDetails />
+            <Permission />
+            <div className="flex gap-8 max-w-[320px] mx-auto">
+              <Button>Save</Button>
+              <Button>Discard</Button>
+            </div>
+          </form>
         </div>
 
-        <div className="max-w-xs w-full">sdsdss</div>
+        <div className="flex flex-col max-w-xs w-full mx-auto gap-8">
+          <UserActivity />
+          <DeactivateUser userId="xsdsd" />
+        </div>
       </div>
     </div>
   )
