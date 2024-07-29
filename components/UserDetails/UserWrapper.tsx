414fe993d35c5035dd5c02121e5ed18028bf2d34
manawara <marcinnawara@int.pl>
2024-07-29 12:53:23 +0200

    Change color text,size,margin for description - UserWrapper

--- components/UserDetails/UserWrapper.tsx
+++ components/UserDetails/UserWrapper.tsx
@@ -7,8 +7,8 @@ const UserWrapper = ({ title, description, children }: UserWrapperProps) => {
     <Card>
       <div className="p-2">
         <h3 className="text-2xl font-semibold">{title}</h3>
-        <span>{description}</span>
-        {children}
+        <span className="text-gray-300 text-sm">{description}</span>
+        <div className="mt-3">{children}</div>
       </div>
     </Card>
   )
